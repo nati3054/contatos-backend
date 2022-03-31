fa69b249073d7922148278335757da34433f3bb8
Sérgio Moura <smouracalmon@gmail.com>
2022-03-31 11:15:22 -0300

    Carregando contatos do back end!

--- public/js/home.js
+++ public/js/home.js
@@ -6,52 +6,6 @@ const search = document.getElementById('search');
 const cancelButton = document.querySelector('#modal button.link');
 const inputParaFocus = document.querySelector('#modal input[name="nome"]')
 
-// Simulando os contatos em uma variável
-const contatos = [
-    {
-        "id":1,
-        "nome":"Kakashi Hatake",
-        "emails":["kakashi@anbu.com"],
-        "telefones":["99999-1111","98888-1234"]
-    },
-    {
-        "id":2,
-        "nome":"Sakura Haruno",
-        "emails":["sakura@konoha.com"],
-        "telefones":["99999-2222","98888-3333"]
-    },
-    {
-        "id":3,
-        "nome":"Hinata Hyuga",
-        "emails":["Hinata@hyugas.com"],
-        "telefones":["99999-3333","98888-4444"]
-    },
-    {
-        "id":4,
-        "nome":"Vovó Tsunade",
-        "emails":["tsunade@hokages.com"],
-        "telefones":["99999-4444","98888-5555"]
-    },
-    {
-        "id":5,
-        "nome":"Shikamaru Nara",
-        "emails":["shikamaru@konoha.com"],
-        "telefones":["99999-5555","98888-6666"]
-    },
-    {
-        "id":6,
-        "nome":"Ino",
-        "emails":["ino@yamanakas.com"],
-        "telefones":["99999-6666","98888-7777"]
-    },
-    {
-        "id":7,
-        "nome":"Choji Akimichi",
-        "emails":["choji@akimichis.com"],
-        "telefones":["99999-7777","98888-8888"]
-    }
-];
-
 const mostrarModal = () => {
     modal.style.display = "flex";
     modal.style.opacity = 1;
@@ -104,7 +58,15 @@ const buscaContatos = trecho => {
     showContatos(contatosFiltrados);
 };
 
-showContatos(contatos);
+const carregaContatos =  async ()=> {
+
+    let resposta = await fetch('/contatos');
+    let contatos = await resposta.json();
+    showContatos(contatos);
+
+}
+
+carregaContatos();
 
 search.addEventListener('keyup', (e) => buscaContatos(e.target.value));
 link.addEventListener('click', mostrarModal);
