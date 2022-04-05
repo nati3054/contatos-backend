98788ec3cb9b9a98f08e7919d386a63ac367f757
Sérgio Moura <smouracalmon@gmail.com>
2022-04-05 12:19:58 -0300

    Criando o token e retornando-o para i visitante

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 const {sequelize} = require('../database/models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const AuthController = {
     login: async (req, res) => {
@@ -8,7 +9,7 @@ const AuthController = {
         let {email, senha} = req.body;
 
         // Levantar do BD o usuário com o email dado
-        let sql = `SELECT id, senha FROM usuarios WHERE email='${email}'`;
+        let sql = `SELECT id, senha, nome FROM usuarios WHERE email='${email}'`;
         let resultados = await sequelize.query(sql, {type:sequelize.QueryTypes.SELECT});
 
         // Caso não haja usuário, retornar erro 403
@@ -19,6 +20,7 @@ const AuthController = {
         // Capturar o id e a senha criptografada, levantados do BD
         let id = resultados[0].id; 
         let senhaCriptografada = resultados[0].senha;
+        let nome = resultados[0].nome;
 
         // Testar a senha do usuário.
         if(!bcrypt.compareSync(senha, senhaCriptografada)) {
@@ -26,10 +28,19 @@ const AuthController = {
             return res.status(403).json({msg:"Falha no login"});
         }
 
-        // TODO: Criar o token
+        // Criar um objeto com os dados úteis do usuário
+        let usuario = {
+            id,
+            nome,
+            email
+        }
 
-        // TODO: Retornar msg de sucesso (200) e o token
-        return res.status(200).json({msg:"Dados de login verificados com sucesso!"});
+        // Criar o token
+        let token = jwt.sign(usuario, "SEGREDO");
+
+        // Retornar msg de sucesso (200) e o token
+        return res.status(200).json({token, usuario});
+
     }
 }
 
