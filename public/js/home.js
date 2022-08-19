956206bf60a566f545f8187007c8357ff7d53df2
Nátalia <nataliaefernanda3054@gmail.com>
2022-08-19 14:12:14 -0300

    login com DOM

--- public/js/home.js
+++ public/js/home.js
@@ -1,147 +1,27 @@
-const loginContainer = document.getElementById("login-container");
-const appContainer = document.getElementById("app-container");
-const formLogin = document.querySelector('#login-container > form');
-const emailLogin = document.querySelector('#email');
-const senhaLogin = document.querySelector('#senha');
-const main = document.querySelector('main');
-const modal = document.getElementById("modal");
-const link = document.getElementById("linkAbrirModal");
-const edits = document.querySelectorAll("section > a");
-const search = document.getElementById('search');
-const cancelButton = document.querySelector('#modal button.link');
-const inputParaFocus = document.querySelector('#modal input[name="nome"]')
+console.log("Ah vei... console.log de novo");
 
-const mostrarModal = () => {
-    modal.style.display = "flex";
-    modal.style.opacity = 1;
-    inputParaFocus.focus()
-};
+//Manipulação de DOM
 
-const esconderModal = (e) => {
-    e.bubbles = false;
-    modal.style.display = "none"
-    modal.style.opacity = 0;
-};
+// capturei o elemento login container 
+const loginContainer = document.getElementById("login-container")
+console.log(loginContainer);
+// alterei a cor de fundo do elemento login-container
+loginContainer.style.backgroundColor = "pink";
 
-const showContatos = contatos => {
-    main.innerHTML = '';
-    contatos.forEach(c => {
-        const section = document.createElement('section');
+// Capture o campo de email e mudem a cor da fonte para vermelho
+const email = document.getElementById("email");
+//mudar a cor da fonte para vermelho (chama pelo o nome da const)
+email.style.color = "grey";
 
-        let htmlEmails = '';
-        c.emails.forEach(e => {
-            htmlEmails += `<a href="mailto:${e}">${e}</a>`;
-        });
-
-        let htmlTels = '';
-        c.telefones.forEach(t => {
-            htmlTels += `<li><a href="tel:${t}">${t}</a></li>`;
-        });
-
-        const html = `
-            <h3>${c.nome}</h3>
-            <div>
-                ${htmlEmails}
-            </div>
-            <ul>
-                ${htmlTels}
-            </ul>
-            <a href="#">Editar</a>
-        `;
-
-        section.innerHTML = html;
-
-        main.appendChild(section);
-    });
-};
-
-const buscaContatos = trecho => {
-    const contatosFiltrados = contatos.filter(
-        c => c.nome.toUpperCase().includes(trecho.toUpperCase())
-    );
-
-    showContatos(contatosFiltrados);
-};
-
-const carregaContatos =  async ()=> {
-
-    // Carregando token do sessionStorage
-    let token = sessionStorage.getItem('token');
-
-    let resposta = await fetch(
-        '/contatos',
-        {
-            method: "GET",
-            headers: {
-                authorization: `bearer ${token}`
-            }
+email.addEventListener(
+    "blur",
+    ()=>{
+        if(email.value == " "){
+            email.style.border = "2px solid red";
+            email.parentNode.innerHTML += "preencha o campo de email"
         }
-    );
-    let contatos = await resposta.json();
-    showContatos(contatos);
-
-}
-
-const login = async dadosDeLogin =>{
-    
-    let response = await fetch(
-        '/login',
-        {
-            method:"POST",
-            body: JSON.stringify(dadosDeLogin),
-            headers: {
-                "content-type":"application/json"
-            }
-        }
-    );
-
-    // Verificando se o login obteve sucesso...
-    if(response.status == 403){
-        
-        alert("Login Inválido");
-        return;
-
-    } else if(response.status == 200){
-
-        // Acessar o conteúdo da reponse
-        let dados = await response.json();
-
-        // Salvar o token
-        sessionStorage.setItem('token', dados.token);
-
-        // Mostrar o app container container e esconder o login;
-        appContainer.style.display = "block";
-        loginContainer.style.display = "none";
-
-        // Carregar os contatos
-        carregaContatos();
-
-    } else {
-
-        alert(`Erro inesperado. Entre em contato com o suporte.\n${response.statusText}`);
-
     }
+)
+const nome = prompt("Digite o seu nome");
+alert(`Bom dia, ${nome}`);
 
-}
-
-search.addEventListener('keyup', (e) => buscaContatos(e.target.value));
-link.addEventListener('click', mostrarModal);
-cancelButton.addEventListener('click', esconderModal);
-modal.addEventListener('keyup', e => e.key === 'Escape' ? esconderModal(e) : null);
-formLogin.addEventListener(
-    'submit',
-    e => {
-        // Interromper o comportamento padrão do evento;
-        e.preventDefault();
-        
-        // Ler os dados de login
-        let dadosDeLogin = {
-            email: emailLogin.value,
-            senha: senhaLogin.value
-        }
-
-        // Chamar uma função para fazer o login
-        login(dadosDeLogin);
-
-    }
-);
